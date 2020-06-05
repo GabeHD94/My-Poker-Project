2c3a621fb22387c095be0f9c05c4afd0c1a53dfc
GabeHD94 <gabehd94@gmail.com>
2020-06-04 23:32:38 -0600

    Making the deck

---
+++ poker.js
@@ -0,0 +1,18 @@
+class mainDeck {
+    constructor() {
+       this.mainDeck = []
+// making the deck
+       const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+       const suits = ['Clubs', 'Diamonds', 'Spades', 'Hearts']
+// assign key value pair, every suit needs one of all values
+       for (const suit in suits) {
+           
+           }
+       }
+    }
+}
+
+const da_deck = new mainDeck();
+
+console.log(da_deck.mainDeck);
+
